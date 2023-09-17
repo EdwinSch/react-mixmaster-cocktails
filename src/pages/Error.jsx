3296f6f6e438210e7b492167d7cc8959817a2d55
front-ed <edwinschutjes@gmail.com>
2023-09-17 16:57:40 +0200

    add: error page + about page + styles

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,25 @@
+import { Link, useRouteError } from "react-router-dom";
+import errorImg from "../assets/not-found.svg";
+
 const Error = () => {
-  return <h2>Error</h2>;
+  const error = useRouteError();
+  // IF 404
+  if (error.status === 404) {
+    return (
+      <div className="error-wrapper">
+        <img src={errorImg} alt="not found" className="img-404" />
+        <h3>page not found</h3>
+        <Link to="/">Back to home</Link>
+      </div>
+    );
+  }
+  // ELSE - other error types
+
+  return (
+    <div className="error-wrapper">
+      <h3>Ohh... something went wrong</h3>;
+    </div>
+  );
 };
 
 export default Error;
