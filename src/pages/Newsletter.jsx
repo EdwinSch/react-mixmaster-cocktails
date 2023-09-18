1e215571aacf0df6f90b082e229d644fde2b2ca1
front-ed <edwinschutjes@gmail.com>
2023-09-18 15:16:30 +0200

    add: newsletter submit form functionality + styles

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,5 +1,78 @@
+import { Form, redirect, useNavigation } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const newsletterUrl = "https://www.course-api.com/cocktails-newsletter";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  // console.log(data);
+
+  try {
+    const response = await axios.post(newsletterUrl, data);
+    // console.log(response);
+    toast.success(response.data.msg);
+    return redirect("/");
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
+
 const Newsletter = () => {
-    return <h2>Newsletter</h2>;
-  };
-  
-  export default Newsletter;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  return (
+    <Form className="form" method="POST">
+      <h5 style={{ marginBottom: "2rem" }}>sign-up for out newletter</h5>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="name"
+          id="name"
+          required
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          last name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="lastName"
+          id="lastName"
+          required
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="email"
+          id="email"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-block"
+        style={{ marginTop: "1rem" }}
+        disabled={isSubmitting}
+      >
+        {isSubmitting ? "submitting" : "submit"}
+      </button>
+    </Form>
+  );
+};
+
+export default Newsletter;
